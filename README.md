810ddf62238ee3a5ff921386601c20f7c38d75b9
Riao Mike <xuruimaths@qq.com>
2022-06-19 21:14:26 +0800

    try again

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # <center>Welcome to Riao</center>
 ## Study Toghter.
+***
+> Now I have changed this file and push it
+> see what will happen.
--- new.c
+++ new.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main(){
+	int i,j;
+	for(i=0;i<10;i++);{
+		printf("I love rui");
+	}
   printf("hello world");
   return 0;
 }
