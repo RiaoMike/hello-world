3d75c49b9c11152cfd9d49baebc2687698ea1bc2
Riao Mike <95469106+RiaoMike@users.noreply.github.com>
2022-06-27 12:28:22 +0800

    Update new.c

--- new.c
+++ new.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 
 int main(){
+	int sun(int, int);
 	int i,j;
+	int a,b;
+	printf("please enter two integer: ");
+	scanf("%d%d",&a,&b);
 	for(i=0;i<10;i++);{
-		printf("I love rui");
+	printf("I love rui");
 	}
-  printf("hello world");
-  return 0;
+  	printf("hello world");
+	printf("sum is %d",sum(a,b));
+  	return 0;
+}
+
+int sum(int a, int b){
+	int s;
+	s=a+b;
+	return s;
 }
